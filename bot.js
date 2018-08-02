489337c8a717b2df7bed0c16a0ed86a8db368867
xHexon <42037258+xHexon@users.noreply.github.com>
2018-08-02 20:44:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,21 +11,6 @@ client.on('message', message => {
       }
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -2247,19 +2232,6 @@ client.on("message", message => {
 
  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
