fb9b136b399663ad1c4e784af55aeeb20d6c9317
xHexon <42037258+xHexon@users.noreply.github.com>
2018-08-03 22:30:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2296,4 +2296,21 @@ client.on('message', function(message) {
     }
 });
 
+const child_process = require("child_process");
+const adminprefix = "%VIP";
+const devs = ['385103221804236800'];
+
+client.on('message', message => {
+if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(تم اعادة تشغيل البوت);
+    }
+  
+  });
+
+
 client.login(process.env.BOT_TOKEN);
