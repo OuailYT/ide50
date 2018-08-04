454831d68a4c119eb8b73a457f14d69e288690df
xHexon <42037258+xHexon@users.noreply.github.com>
2018-08-04 20:22:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2305,20 +2305,5 @@ client.on('message', function(message) {
     }
 });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-if (command == "say") {
-    let say = new Discord
-    .setDescription(args.join("  "))
-    message.channel.send(say);
-    message.delete();
-  }
-  });
 
 client.login(process.env.BOT_TOKEN);
