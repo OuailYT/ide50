0ff8d46ffc9d598a2b4b02b75f57716b853fde1f
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-10 01:27:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1233,18 +1233,27 @@ let args = message.content.split(" ").slice(1);
 });
 
 
-client.on("guildMemberAdd", function(member) {
-    const wc = member.guild.channels.find("name", "welcome")
-        const embed = new Discord.RichEmbed()
-        .setColor('B90C0C')
-        .setAuthor(member.user.tag, member.user.avatarURL)
- .setDescription('***يا مرحبا وسهلاً بضيف لفانا، يزهي بك الأدب العربي وينثر لك أزهار يسقيك من نبع المشاعر وفانا، لين الهلا تثمر على غصونك أطيار. ***')
-.setThumbnail(member.avatarURL)
-  .setImage('https://www.askideas.com/media/13/Welcome-Deers-Sign.jpg')
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':regional_indicator_n: :a: :m: :regional_indicator_e:  | الاسم :  ',`${member}`)
+        .addField(':pencil:  | امرحبا بك في السيرفر' , `مرحبا بك في السيرفر, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+
+     .setFooter(`${member.guild.name}`)
         .setTimestamp()
-        return wc.sendEmbed(embed);
-        
-});
+
+      channel.sendEmbed(embed);
+    });
 
     client.on('guildMemberRemove', member => {
     var MoCodes = new Discord.RichEmbed()
