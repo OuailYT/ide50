d00a54698e0fd5c41ec4dd03d75bcf48675bf8a6
xBacose <42037258+xBacose@users.noreply.github.com>
2018-08-15 00:47:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1201,7 +1201,6 @@ msg.channel.send(`${item.type}`).then(() => {
             userData.points++;
           })
           .catch(collected => {
-            message.channel.send(`:x: **خطأ حاول مرة اخرى**`);
             console.log('[Typing] Error: No one type the word.');
           })
         })
@@ -1213,10 +1212,11 @@ msg.channel.send(`${item.type}`).then(() => {
 client.on('message', message => {
 if (message.content.startsWith(prefix + 'نقاطي')) {
     if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+    const type = require('./fkk/fkkPTS.json');
     let userData = points[message.author.id];
     let embed = new Discord.RichEmbed()
     .setAuthor(`${message.author.tag}`, message.author.avatarURL)
-    .setColor('#000000')
+    .setColor('RANDOM')
     .setDescription(`نقاطك: \`${userData.points}\``)
     message.channel.sendEmbed(embed)
   }
